d2fe55df459d65a733c3ca45576f6373f3d782a4
chamoo334 <59486871+chamoo334@users.noreply.github.com>
2021-03-15 00:15:26 -0700

    File practice and testing.

---
+++ file_test/file_prac.py
@@ -0,0 +1,18 @@
+# import os
+# print ("cwd" + os.getcwd())
+
+
+# open and copy
+f = open("file_test.vtt", "r")
+contents = f.read().splitlines()
+f.close()
+
+print(contents)
+
+# remove formatting
+
+
+
+# replace words and save
+
+
