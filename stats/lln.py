ce8654c30d2a7d2f322e6b0e9ed5dfa8c3a752af
chamoo334 <59486871+chamoo334@users.noreply.github.com>
2021-03-26 14:35:43 -0700

    completed graphing of general population data and sample data.

--- stats/lln.py
+++ stats/lln.py
@@ -1,17 +1,36 @@
-
+# Using graphs to examine the law of large numbers and how 
+# it differs from central limit theory
 
 import matplotlib.pyplot as plt
 import numpy as np
 
 # parameters
-pop_size = 2.0e5
+population_size = 2.0e5
+sample_size = 50
+num_of_samples = 500
 
 
 # generate shuffled sample population data and plot
-population = 1 / np.logspace(np.log10(.001),np.log10(5),int(pop_size))
+population = 1 / np.logspace(np.log10(.001),np.log10(5),int(population_size))
 np.random.shuffle(population)
-plt.plot(population[::int(1e3)], 'o')
-plt.xlabel('sample')
-plt.ylabel('Data value')
-plt.show()
+plt.plot(population[::int(1e3)], 'o', label='Population')
+plt.title("Population", fontsize=14)
+plt.xlabel('Sample Number')
+plt.ylabel('Data Value')
+plt.show(block=False)
+plt.pause(3.5)
+plt.close()
 
+# obtaining multiple sample means via monte carlo samplimg 
+# (random sampling from the population)
+sample_means = [np.mean(np.random.choice(population, size=sample_size)) for expirement in range(num_of_samples)]
+trueMean = np.mean(population)
+plt.plot(sample_means, 'ko', label='Sample Means')
+plt.title("Sample Means", fontsize=14)
+plt.plot([0, num_of_samples], [trueMean, trueMean], 'r', linewidth=5, label='True mean')
+plt.xlabel('sample Number')
+plt.ylabel('Mean Value')
+plt.legend()
+plt.show(block=False)
+plt.pause(3.5)
+plt.close()
