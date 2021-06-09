d38453c2709a4687d57e4325bcd77fa3632bfb8f
chamoo334 <moorchan@engr.orst.edu>
2021-06-08 21:56:09 -0700

    Algebra subfolders combined to main and added functions to polynmial.py

--- algebra/polynomials.py
+++ algebra/polynomials.py
@@ -1,4 +1,5 @@
 import sympy as sym
+import numpy as np
 
 def add_polys(poly_a, poly_b):
     """ """
@@ -26,13 +27,18 @@ def div_polys(poly_a, poly_b):
 
 def factor_poly(equation_str):
     """ Returns the factored form """
-    expr = sym.sympify(equation_str)
-    return sym.factor(expr)
+    return sym.factor(sym.sympify(equation_str))
 
 
 def get_discrim(poly_a):
     """ Return the discriminant of a polynomial """
     return sym.discriminant(poly_a)
+
+
+def get_poly_roots(poly_a):
+    """ Returns a list that contains the roots of a polynomial """
+    expr = sym.sympify(poly_a).as_poly()
+    return np.roots(expr.all_coeffs()).tolist()
 
 
 # sym.pprint(factor_poly("-2*x**2 + 7*x -6"))
