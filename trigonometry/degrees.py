8cb3728b70f1d09f2aa75dda4349fd8cd61200c2
chamoo334 <moore.cmarie@gmail.com>
2021-06-10 12:56:54 -0700

    degrees.py for converting between rad and deg

---
+++ trigonometry/degrees.py
@@ -0,0 +1,12 @@
+import numpy as np
+
+
+def deg_2_rad(deg, bounded=True):
+    """ Returns radian value from deg. Bounded=True for 0 to 2pi. """
+    if not bounded:
+        return np.deg2rad(deg)
+    return np.deg2rad(deg) % (2*np.pi)
+
+def rad_2_deg(rad):
+    """ Returns degree value from rad. """
+    return np.rad2deg(rad)
